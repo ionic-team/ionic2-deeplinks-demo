2f2f12ea66ae469f0632793ce5a2b94086471805
Max Lynch <max@drifty.com>
2016-06-06 15:41:29 -0500

    Workin and twerkin

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,7 @@ import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 
+declare var IonicDeeplink:any;
 
 @App({
   template: '<ion-nav [root]="rootPage"></ion-nav>',
@@ -15,6 +16,17 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+
+      console.log('Registering for deeplinks');
+      IonicDeeplink.onDeepLink((event) => {
+        console.log('Ionic DEEPLINK', event.url);
+      })
+      IonicDeeplink.onDeepLink((event) => {
+        console.log('Ionic DEEPLINK #2', event.url);
+      })
+      IonicDeeplink.onDeepLink((event) => {
+        console.log('Ionic DEEPLINK #3', event.url);
+      })
     });
   }
 }
