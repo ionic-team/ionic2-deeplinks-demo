4552ff11afe55956c1a26b7cacd9c5eb7a952918
Max Lynch <max@drifty.com>
2016-06-06 18:29:54 -0500

    Params

--- app/app.ts
+++ app/app.ts
@@ -21,9 +21,6 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-
-      console.log('Registering for deeplinks');
-
     });
   }
 
--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -1,10 +1,12 @@
-import {Page} from 'ionic-angular';
+import {Page, NavParams} from 'ionic-angular';
 
 @Page({
   templateUrl: 'build/pages/about/about.html'
 })
 export class AboutPage {
-  constructor() {
-
+  thing: string;
+  
+  constructor(navParams: NavParams) {
+    this.thing = navParams.get('t');
   }
 }
